09acbfeb0be3d0f643251f6f5480fb6ea3dbeed3
Carlos Florencio <carlosmflorencio@gmail.com>
2016-01-12 00:44:21 +0000

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Stream
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Team";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Result:";
+            this.label2.Visible = false;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(67, 37);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 62);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Auto Open Stream With Chrome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stream
+{
+    public partial class Form1 : Form
+    {
+        private static string URL = "http://ifirstrowpt.eu/";
+        private HashSet<String> links = new HashSet<String>();
+        private int currentStream = 0;
+        private string oldTeam = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        // Handles the button click
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startLooking();
+        }
+
+        private void startLooking()
+        {
+            String team = textBox1.Text;
+
+            if (team.Length == 0)
+            {
+                MessageBox.Show("Team must not be empty..");
+                return;
+            }
+
+            if (links.Count != 0)
+            {
+                currentStream = ++currentStream % links.Count;
+                button1.Text = getButtonString();
+                startStream();
+                return;
+            }
+
+            team = parseTeamName(team);
+            oldTeam = team;
+
+            String html = getHtml(URL);
+
+            getLinks(team, html);
+
+            label2.Visible = true;
+            labelResult.Text = links.Count + " Streams found.";
+
+            if (links.Count != 0)
+            {
+                button1.Text = getButtonString();
+            }
+
+            startStream();
+        }
+
+        private string getButtonString()
+        {
+            if(links.Count == 0)
+            {
+                return "Start!";
+            }
+            else
+            {
+                return "Next " + (currentStream+1) + "/" + links.Count;
+            }
+        }
+
+        private void startStream()
+        {
+            if (links.Count == 0)
+            {
+                MessageBox.Show("No streams found..");
+                return;
+            }
+
+            Process.Start("chrome", @"--app=" + URL + links.ElementAt(currentStream));
+        }
+
+        // Parses the team name
+        private string parseTeamName(string team)
+        {
+            return team.ToLower().Replace(" ", "-");
+        }
+
+        // Gets the html of a page
+        private String getHtml(String url)
+        {
+            String data = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                System.IO.Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();      
+            }
+
+            return data;
+        }
+
+        // Gets a list of links from the received html containing the receveid team
+        private void getLinks(String team, String html)
+        {
+            string pat = @"(\/watch\/\d+\/\d\/\watch-.*?" + team + ".*?\\.html)";
+
+            Regex r = new Regex(pat, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            // Match the regular expression pattern against a text string.
+            Match m = r.Match(html);
+            int matchCount = 0;
+            while (m.Success)
+            {
+                Group g = m.Groups[1];
+                links.Add(g.Value);
+                m = m.NextMatch();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Text = "Start!";
+            labelResult.Text = "";
+            label2.Visible = false;
+            currentStream = 0;
+            links = new HashSet<string>();
+        }
+
+      
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Enter)
+            {
+                startLooking();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
