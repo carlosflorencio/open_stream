25e448f4b488cca6e685e1341e217a6636a4c149
no0law1 <nuno.m.reis@netcabo.pt>
2016-01-14 02:23:31 +0000

    improved UI

---
+++ Stream.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Stream
+{
+    partial class StreamView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pinned_team = new System.Windows.Forms.Label();
+            this.team = new System.Windows.Forms.TextBox();
+            this.link_list = new System.Windows.Forms.ListView();
+            this.link = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // pinned_team
+            // 
+            this.pinned_team.AutoSize = true;
+            this.pinned_team.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pinned_team.Location = new System.Drawing.Point(12, 9);
+            this.pinned_team.Name = "pinned_team";
+            this.pinned_team.Size = new System.Drawing.Size(42, 13);
+            this.pinned_team.TabIndex = 0;
+            this.pinned_team.Text = "Team:";
+            // 
+            // team
+            // 
+            this.team.Location = new System.Drawing.Point(60, 6);
+            this.team.Name = "team";
+            this.team.Size = new System.Drawing.Size(212, 20);
+            this.team.TabIndex = 1;
+            this.team.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.team_KeyPress);
+            // 
+            // link_list
+            // 
+            this.link_list.Activation = System.Windows.Forms.ItemActivation.TwoClick;
+            this.link_list.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.link});
+            this.link_list.FullRowSelect = true;
+            this.link_list.Location = new System.Drawing.Point(15, 32);
+            this.link_list.MultiSelect = false;
+            this.link_list.Name = "link_list";
+            this.link_list.Size = new System.Drawing.Size(257, 217);
+            this.link_list.TabIndex = 2;
+            this.link_list.UseCompatibleStateImageBehavior = false;
+            this.link_list.View = System.Windows.Forms.View.List;
+            this.link_list.DoubleClick += new System.EventHandler(this.link_list_DoubleClick);
+            // 
+            // link
+            // 
+            this.link.Text = "Link";
+            this.link.Width = 241;
+            // 
+            // StreamView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.link_list);
+            this.Controls.Add(this.team);
+            this.Controls.Add(this.pinned_team);
+            this.Name = "StreamView";
+            this.Text = "Stream";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pinned_team;
+        private System.Windows.Forms.TextBox team;
+        private System.Windows.Forms.ListView link_list;
+        private System.Windows.Forms.ColumnHeader link;
+    }
+}
---
+++ Stream.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Stream
+{
+    public partial class StreamView : Form
+    {
+
+        private class Stream
+        {
+            public string title;
+            public string link;
+
+            public Stream(string title, string link)
+            {
+                this.title = title;
+                this.link = link;
+            }
+        }
+
+        private static string URL = "http://ifirstrowpt.eu/";
+        private LinkedList<Stream> links = new LinkedList<Stream>();
+        private int currentStream = 0;
+        private string oldTeam = "";
+
+        public StreamView()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void team_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.Equals(Convert.ToChar(Keys.Enter)))
+            {
+                links = new LinkedList<Stream>();
+                link_list.Items.Clear();
+                startLooking();
+            }
+        }
+
+        private void startLooking()
+        {
+            String teamString = team.Text;
+
+            if (teamString.Length == 0)
+            {
+                MessageBox.Show("Team must not be empty..");
+                return;
+            }
+
+            teamString = parseTeamName(teamString);
+            oldTeam = teamString;
+
+            String html = getHtml(URL);
+
+            getLinks(teamString, html);
+            if (links.Count == 0)
+            {
+                MessageBox.Show("No streams found..");
+            }
+            else
+            {
+                fillListView();
+            }
+        }
+
+        private void fillListView()
+        {
+            int i=1;
+            foreach (var streamLink in links)
+            {
+                string[] row = { streamLink.title, streamLink.link };
+                var listViewItem = new ListViewItem(row);
+                link_list.Items.Add(listViewItem);
+                i++;
+            }
+        }
+
+        private void startStream(int idx)
+        {
+            Process.Start("chrome", @"--app=" + URL + links.ElementAt(idx).link);
+        }
+
+        // Parses the team name
+        private string parseTeamName(string team)
+        {
+            return team.ToLower().Replace(" ", "-");
+        }
+
+        // Gets the html of a page
+        private String getHtml(String url)
+        {
+            String data = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                System.IO.Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+
+            return data;
+        }
+
+        // Gets a list of links from the received html containing the receveid team
+        private void getLinks(String team, String html)
+        {
+            string pat = @"(\/watch\/\d+\/\d\/\watch-.*?" + team + ".*?\\.html)";
+
+            Regex r = new Regex(pat, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            // Match the regular expression pattern against a text string.
+            Match m = r.Match(html);
+            while (m.Success)
+            {
+                Group g = m.Groups[1];
+                int startIndex = g.Value.LastIndexOf("watch-") + "watch-".Length;
+                int length = g.Value.IndexOf(".", startIndex) - startIndex;
+                string title = length < 0 ? "Unknown" : g.Value.Substring(startIndex, length);
+                links.AddLast(new Stream(title, g.Value));
+                m = m.NextMatch();
+            }
+        }
+
+        private void link_list_DoubleClick(object sender, EventArgs e)
+        {
+            int idx = link_list.SelectedItems[0].Index;
+            startStream(idx);
+        }
+    }
+}
